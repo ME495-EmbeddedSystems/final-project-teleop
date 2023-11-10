c7bcb7a8ea405ca45e87b14da884b573dd441bf2
rohankota <rohankota@u.northwestern.edu>
2023-11-10 15:26:54 -0600

    random tweaks

--- teleop_haptics/teleop_haptics/haptx_feedback.py
+++ teleop_haptics/teleop_haptics/haptx_feedback.py
@@ -42,6 +42,19 @@ class HaptxFeedback(Node):
         self.inflation = 0.0
         self.dir = 'up'
 
+        # Center tactors
+        self.rh_th_center_tactors = [0061, 0071, 0081, 0031, 0041, 0101, 0091]
+        self.rh_ff_center_tactors = [1061, 1071, 1081, 1031, 1041, 1101, 1091]
+        self.rh_mf_center_tactors = [2061, 2071, 2081, 2031, 2041, 2101, 2091]
+        self.rh_rf_center_tactors = [3032, 3031, 3022, 3052]
+        self.rh_lf_center_tactors = [4032, 4031, 4022, 4052]
+
+        self.lh_th_center_tactors = [10061, 10071, 10081, 10031, 10041, 10101, 10091]
+        self.lh_ff_center_tactors = [11061, 11071, 11081, 11031, 11041, 11101, 11091]
+        self.lh_mf_center_tactors = [12061, 12071, 12081, 12031, 12041, 12101, 12091]
+        self.lh_rf_center_tactors = [13032, 13031, 13022, 13052]
+        self.lh_lf_center_tactors = [14032, 14031, 14022, 14052]
+
     def timer_callback(self):
         """Timer function for the Haptx Feedback node."""
         msg = TactorGroupState()
