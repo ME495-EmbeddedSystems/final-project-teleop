19924e05cfc0848ba34909f884124712a950a453
leo-chen <S8O9R6@u.northwestern.edu>
2023-11-15 16:54:50 -0600

    April tag rect working now.

--- teleop_sensing/setup.py
+++ teleop_sensing/setup.py
@@ -19,7 +19,6 @@ setup(
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-        ],
+        'console_scripts': ['cv_process = teleop_sensing.cv_process:main'],
     },
 )
