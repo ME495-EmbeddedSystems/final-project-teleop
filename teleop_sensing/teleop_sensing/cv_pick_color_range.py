3afb6e4abcb52d1a743db6a8d5d721cf4a2e700a
leo-chen <S8O9R6@u.northwestern.edu>
2023-11-16 10:16:39 -0600

    Reflection into base class , Find object working with depth image

--- teleop_sensing/teleop_sensing/cv_pick_color_range.py
+++ teleop_sensing/teleop_sensing/cv_pick_color_range.py
@@ -3,7 +3,7 @@ from typing import Any
 import rclpy
 from rclpy.node import Node as RosNode
 
-from sensor_msgs.msg import Image as RosImage , CompressedImage
+from sensor_msgs.msg import Image as RosImage, CompressedImage
 
 from cv_bridge import CvBridge
 import dataclasses
@@ -12,25 +12,13 @@ import cv2
 import typing
 import functools
 import numpy as np
+import dataclass_wizard
+
+
 @dataclasses.dataclass
-class HSVFilter():
+class FilterBase(dataclass_wizard.JSONFileWizard):
 
-    h_high:float= 179
-    h_low:float= 0
-    s_high:float= 255
-    s_low:float= 0
-    v_high:float= 255
-    v_low:float= 0
-
-    # The exact names of these const are referenced later.
-    H_MAX: typing.ClassVar[float] = 179
-    H_MIN: typing.ClassVar[float] = 0
-    S_MAX: typing.ClassVar[float] = 255
-    S_MIN: typing.ClassVar[float] = 0
-    V_MAX: typing.ClassVar[float] = 255
-    V_MIN: typing.ClassVar[float] = 0
-
-    def get_field_range(self,field_name:str)->tuple[float,float]:
+    def get_field_range(self, field_name: str) -> tuple[float, float]:
         """Get the possible min and max of a field
 
         Args:
@@ -40,34 +28,151 @@ class HSVFilter():
             ValueError: if the name of the field is not in the class
         """
         if field_name not in [f.name for f in dataclasses.fields(self)]:
-            raise ValueError(f"Trying to get the range of a field: {field_name} that doesn't exists in data class")
+            raise ValueError(
+                f"Trying to get the range of a field: {field_name} that doesn't exists in data class"
+            )
 
         # Have a strong coupling of the namings of variables
-        return (getattr(self,field_name[0].upper() +"_MAX"),getattr(self,field_name[0].upper() +"_MIN"))
+        return (getattr(self,
+                        field_name.split("_")[0].upper() + "_MAX"),
+                getattr(self,
+                        field_name.split("_")[0].upper() + "_MIN"))
 
-    def update_field_by_name(self , field_name:str , value:float)->str:
-        
-        x_max , x_min = self.get_field_range(field_name)
-        value = min(x_max , max(value , x_min))
+    def update_field_by_name(self, field_name: str, value: float) -> list[str]:
+        x_max, x_min = self.get_field_range(field_name)
+        value = min(x_max, max(value, x_min))
         setattr(self, field_name, value)
+        return []
 
-        channel , side = field_name.split("_")
+    def apply_filter(self, bgr_image):
+        return bgr_image
+
+    def ensure_linked_field_smaller(self, field, linked_field, margin=1):
+        if ((getattr(self, field) - getattr(self, linked_field)) < margin):
+            # also update counter part
+            setattr(self, linked_field, getattr(self, field) - margin)
+            return True
+        return False
+
+    def ensure_linked_field_larger(self, field, linked_field, margin=1):
+        if ((getattr(self, linked_field) - getattr(self, field)) < margin):
+            # also update counter part
+            setattr(self, linked_field, getattr(self, field) + margin)
+            return True
+        return False
+
+
+@dataclasses.dataclass
+class HSVFilter(FilterBase):
+
+    h_high: float = 179
+    h_low: float = 0
+    s_high: float = 255
+    s_low: float = 60
+    v_high: float = 250
+    v_low: float = 40
+
+    # The exact names of these const are referenced later.
+    H_MAX: typing.ClassVar[float] = 179
+    H_MIN: typing.ClassVar[float] = 0
+    S_MAX: typing.ClassVar[float] = 255
+    S_MIN: typing.ClassVar[float] = 0
+    V_MAX: typing.ClassVar[float] = 255
+    V_MIN: typing.ClassVar[float] = 0
+
+    def update_field_by_name(self, field_name: str, value: float) -> list[str]:
+        super().update_field_by_name(field_name, value)
+
+        # The custom linked attribute updating
+        channel, side = field_name.split("_")
         if side == "high":
             counter_part = channel + "_low"
-            if(getattr(self,counter_part) > getattr(self,field_name)):
+            if (getattr(self, counter_part) > getattr(self, field_name)):
                 # also update counter part
-                setattr(self,counter_part,value)
+                setattr(self, counter_part, value)
         else:
             counter_part = channel + "_high"
-            if(getattr(self,counter_part) < getattr(self,field_name)):
+            if (getattr(self, counter_part) < getattr(self, field_name)):
                 # also update counter part
-                setattr(self,counter_part,value)
-        return counter_part
+                setattr(self, counter_part, value)
+        return [counter_part]
 
-    def apply_filter(self,bgr_image):
+    def apply_filter(self, bgr_image):
         hsv_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2HSV)
         return cv2.inRange(hsv_image, (self.h_low, self.s_low, self.v_low),
                            (self.h_high, self.s_high, self.v_high))
+
+
+@dataclasses.dataclass
+class BlobDetector(FilterBase):
+    # open_size:float = 0
+
+    maxThreshold: int = 101
+    minThreshold: int = 11
+
+    maxArea: int = 5999
+    minArea: int = 601
+
+    MAXTHRESHOLD_MAX: typing.ClassVar[float] = 250
+    MAXTHRESHOLD_MIN: typing.ClassVar[float] = 30
+
+    MINTHRESHOLD_MAX: typing.ClassVar[float] = 230
+    MINTHRESHOLD_MIN: typing.ClassVar[float] = 2
+
+    MAXAREA_MAX: typing.ClassVar[float] = 10000
+    MAXAREA_MIN: typing.ClassVar[float] = 1500
+
+    MINAREA_MAX: typing.ClassVar[float] = 3000
+    MINAREA_MIN: typing.ClassVar[float] = 100
+
+    # Overriding init for dataclass is bad
+    def __post_init__(self):
+        super().__init__()
+
+        self.params = cv2.SimpleBlobDetector_Params()
+
+        self.detector = cv2.SimpleBlobDetector_create(self.params)
+
+    def update_field_by_name(self, field_name: str, value: float) -> list[str]:
+
+        super().update_field_by_name(field_name, value)
+
+        setattr(self.params, field_name, getattr(self, field_name))
+        self.detector = cv2.SimpleBlobDetector_create(self.params)
+
+        if field_name == "maxThreshold":
+            if self.ensure_linked_field_smaller(field_name, "minThreshold", 2):
+                return ["minThreshold"]
+
+        if field_name == "minThreshold":
+            if self.ensure_linked_field_larger(field_name, "maxThreshold", 2):
+                return ["maxThreshold"]
+
+        if field_name == "maxArea":
+            if self.ensure_linked_field_smaller(field_name, "minArea", 10):
+                return ["minArea"]
+
+        if field_name == "minArea":
+            if self.ensure_linked_field_larger(field_name, "maxArea", 10):
+                return ["maxArea"]
+        return []
+
+    def apply_filter(self, bgr_image):
+        keypoints = self.detector.detect(bgr_image)
+
+        for keypoint in keypoints:
+            x, y = keypoint.pt
+            x = int(x)
+            y = int(y)
+            print(f"have keypoint at:{x} , {y}")
+            print(f"angle {keypoint.angle}")
+
+            cv2.circle(bgr_image, (x, y), 5, (100, 100, 255), -1)
+
+        im_with_keypoints = cv2.drawKeypoints(bgr_image, keypoints, np.array([]), (0, 0, 255),
+                                              cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+        return im_with_keypoints , keypoints
 
 
 def main(args=None):
@@ -97,36 +202,45 @@ class CvColorFilterFinder(RosNode):
 
         self.hsv_filter = HSVFilter()
 
-        self.img_subscriber = self.create_subscription(RosImage
-         , "/image_rect_color" , self.img_recv_callback , 10)
-        self.br = CvBridge()
-        cv2.namedWindow(self.WINDOW_NAME )
+        self.blob_detector = BlobDetector()
+        print(self.blob_detector)
 
-        self.setup_cv_trackbar()
+        self.img_subscriber = self.create_subscription(RosImage, "/image_rect_color",
+                                                       self.img_recv_callback, 10)
+        self.br = CvBridge()
+        cv2.namedWindow(self.WINDOW_NAME, cv2.WINDOW_NORMAL)
+
+        self.setup_cv_trackbar(self.hsv_filter)
+        self.setup_cv_trackbar(self.blob_detector)
 
     # Yes, Intentionally stuff everything into the callback.
     # Color picking only need to update when we have new image anyway
-    def img_recv_callback(self , msg:RosImage):
+    def img_recv_callback(self, msg: RosImage):
         # print(f" Header  {msg.header} Encoding  {msg.encoding} \n"
         # +f"Height {msg.height} Width  {msg.width} Step  {msg.step}")
         # Convert the message to a new image
-        bgr_input_image = self.br.imgmsg_to_cv2(msg , desired_encoding="bgr8")
-
+        bgr_input_image = self.br.imgmsg_to_cv2(msg, desired_encoding="bgr8")
 
         hsv_mask = self.hsv_filter.apply_filter(bgr_input_image)
-        masked_image = cv2.bitwise_and(bgr_input_image,bgr_input_image , mask=hsv_mask)
+        masked_image = cv2.bitwise_and(bgr_input_image, bgr_input_image, mask=hsv_mask)
 
-        cv2.imshow(self.WINDOW_NAME, np.hstack([bgr_input_image , masked_image]) )
+        blob_marked = self.blob_detector.apply_filter(masked_image)
+
+        stacked_image = np.hstack([bgr_input_image, blob_marked])
+        scaled_image = self.scale_image_down(stacked_image)
+        cv2.imshow(self.WINDOW_NAME, scaled_image)
         cv2.waitKey(50)
+        # TODO
+        # self.hsv_filter.to_json_file()
 
+    def scale_image_down(self, image: np.ndarray):
+        height, width, _ = image.shape
+
+        scale_down_factor = 1800 / width
+        return cv2.resize(image, (0, 0), fx=scale_down_factor, fy=scale_down_factor)
 
     # Node: the order of arguments matter.
-    def update_field_callback(self,field_name:str , new_value):
-        # if field_name not in dataclasses.fields(self):
-        #     raise ValueError("Trying to update a field that doesn't exists in data class")
-
-        counter_part_name = self.hsv_filter.update_field_by_name(field_name, new_value)
-
+    def update_field_callback(self, filter: FilterBase, field_name: str, new_value):
         # In theory this is actually very very bad. But in practice it worked.
         # Problem with cv2 setTrackbarPos is it also trigger callback (instead of
         # just user hitting it)
@@ -134,39 +248,46 @@ class CvColorFilterFinder(RosNode):
         # In practice, the callback is only called when value is changed.
         # So what will happen is a callback to change h_high will not re-trigger itself
         # internally. However this this h_high callback also change h_low, it will
-        # trigger a h_low callback. Luckly, since value is set, setting h_low won't trigger 
+        # trigger a h_low callback. Luckly, since value is set, setting h_low won't trigger
         # another h_high callback
 
-        actual_set_value = getattr(self.hsv_filter , field_name)
-        cv2.setTrackbarPos(field_name,self.WINDOW_NAME, actual_set_value)
-
-        counter_part_value = getattr(self.hsv_filter , counter_part_name)
-        cv2.setTrackbarPos(counter_part_name,self.WINDOW_NAME, counter_part_value)
-
+        linked_attrs = filter.update_field_by_name(field_name, new_value)
+        actual_set_value = getattr(filter, field_name)
         print(f"Setting {field_name} to {actual_set_value}")
+        cv2.setTrackbarPos(field_name, self.WINDOW_NAME, actual_set_value)
 
+        for linked_attr in linked_attrs:
+            print(f"Also update linked attr {linked_attr}")
+            linked_value = getattr(filter, linked_attr)
+            cv2.setTrackbarPos(linked_attr, self.WINDOW_NAME, linked_value)
 
-    def setup_cv_trackbar(self):
-
-        def nothing(value):
-            print(f"called {value}")
-        cv2.createTrackbar("a" , self.WINDOW_NAME, 0,124,nothing)
-
-        for field in dataclasses.fields(self.hsv_filter):
+    def setup_cv_trackbar(self, filter: FilterBase):
+        default_value_map = {}
+        for field in dataclasses.fields(filter):
             name = field.name
-            field_max , field_min = self.hsv_filter.get_field_range(name)
+            print(f"\n-------\n  Adding field {name}")
+            field_max, field_min = filter.get_field_range(name)
             # Shouldn't use lambda here, as lambda creates links not copies
             # binded_callback = lambda value: self.update_field_callback(name,value)
 
             # Note this binding is in orders, from left to right.
-            binded_callback = functools.partial(self.update_field_callback, name)
+            binded_callback = functools.partial(self.update_field_callback, filter, name)
 
-            cv2.createTrackbar(name,self.WINDOW_NAME , field_min,
-                           field_max, binded_callback)
-            cv2.setTrackbarPos(name,self.WINDOW_NAME, getattr(self.hsv_filter , name))
-            print(f"Added trackbar for {name}")
+            print(f"max min {field_max} , {field_min}\n{binded_callback}")
 
+            # When creating trackbar with min bigger then 0, or max smaller then 0.
+            # the creating process will call setTrackbar to snap it to limit, but
+            # will trigger a callback. Thus
+            # The default value needs to to be remembered.
+            default_value_map[name] = getattr(filter, name)
+            # print(f"callback {binded_callback}")
+            cv2.createTrackbar(name, self.WINDOW_NAME, field_min, field_max, binded_callback)
 
+        for field in dataclasses.fields(filter):
+            # Must do this after all bars being done. Or the set trackbar could trigger
+            # a callback which cause the linked_attr's trackbar (not existing) to be sets
+            print(f"-- Initializeing {field.name} to {default_value_map[field.name]} ")
+            cv2.setTrackbarPos(field.name, self.WINDOW_NAME, default_value_map[field.name])
 
 
 if __name__ == '__main__':
--- teleop_sensing/teleop_sensing/cv_process.py
+++ teleop_sensing/teleop_sensing/cv_process.py
@@ -1,11 +1,27 @@
-import rclpy
-from rclpy.node import Node as RosNode
-
-from sensor_msgs.msg import Image as RosImage , CompressedImage
-
-from cv_bridge import CvBridge
+#! /usr/bin/env python3
+import collections
+import dataclasses
+import functools
 
 import cv2
+import numpy as np
+# Copied from example.
+import pyrealsense2 as rs2
+import rclpy
+from cv_bridge import CvBridge
+from cv_pick_color_range import BlobDetector, FilterBase, HSVFilter
+from rclpy.node import Node as RosNode
+from rclpy.time import Time as RosTime
+from rclpy.wait_for_message import wait_for_message
+from sensor_msgs.msg import CameraInfo, CompressedImage
+from sensor_msgs.msg import Image as RosImage
+from sensor_msgs.msg import PointCloud2
+from geometry_msgs.msg import TransformStamped , Transform , Vector3 , Point as PointMsg
+
+if (not hasattr(rs2, 'intrinsics')):
+    import pyrealsense2.pyrealsense2 as rs2
+
+from tf2_ros import TransformBroadcaster
 
 
 def main(args=None):
@@ -23,6 +39,13 @@ def main(args=None):
 
 
 class ImageProcesser(RosNode):
+    WINDOW_NAME = "detection window"
+
+    class TimedKeypoints():
+        time_stamp:float
+        keypoints :list
+
+    
 
     def __init__(self) -> None:
         super().__init__("image_process")
@@ -30,24 +53,174 @@ class ImageProcesser(RosNode):
         self.debug = self.get_logger().debug
         self.warn = self.get_logger().warning
         self.error = self.get_logger().error
+        self.info("Camera info is setup. Assume it never changes")
+        cv2.namedWindow(self.WINDOW_NAME, cv2.WINDOW_NORMAL)
+        self.hsv_filter = HSVFilter()
+        self.blob_detector = BlobDetector()
 
-        self.img_subscriber = self.create_subscription(RosImage
-         , "/image_rect_color" , self.img_recv_callback , 10)
+        self.setup_cv_trackbar(self.blob_detector)
+        self.setup_cv_trackbar(self.hsv_filter)
+
         self.br = CvBridge()
+
+        self.depth_topic = "/D435i/aligned_depth_to_color/image_raw"
+
+        self.intrinsics = self.record_camera_info_once()
+
+        self.timed_keypoints = ImageProcesser.TimedKeypoints()
+        # store 5 image to past
+        self.depth_image_deque = collections.deque(maxlen= 3)
+
+        # self.depth_image_sub = self.create_subscription(RosImage, "/D435i/depth/image_rect_raw", self.depth_image_cb,10)
+        self.depth_image_sub = self.create_subscription(RosImage, self.depth_topic, self.depth_image_cb,10)
+        self.img_subscriber = self.create_subscription(RosImage, "/image_rect_color",self.color_img_recv_callback, 10)
+
+        # self.process_timer = self.create_timer(1/50 ,self.find_ring_process_timercb)
+
+        self.transform_broadcaster = TransformBroadcaster(self)
+
+    def color_img_recv_callback(self, msg: RosImage):
+
+        bgr_input_image = self.br.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+
+        hsv_mask = self.hsv_filter.apply_filter(bgr_input_image)
+
+        kernel = np.ones((5,5),np.uint8)
+        closed_mask = cv2.morphologyEx(hsv_mask, cv2.MORPH_CLOSE, kernel)
+        opened_mask = cv2.morphologyEx(closed_mask, cv2.MORPH_OPEN, kernel)
+        masked_image = cv2.bitwise_and(bgr_input_image, bgr_input_image, mask=opened_mask)
+
+        blob_marked , keypoints = self.blob_detector.apply_filter(masked_image)
         
-    def img_recv_callback(self , msg:RosImage):
-        print(f"""
-Header  {msg.header}")
-Encoding  {msg.encoding}
-Height  {msg.height}
-Width  {msg.width}
-Step  {msg.step}
-""")        
-        # Convert the message to a new image
-        im2 = self.br.imgmsg_to_cv2(msg , desired_encoding="bgr8") 
-        # print(f"Shape of compressed {im2}")
-        cv2.imshow("image", im2)
+        cv2.imshow(self.WINDOW_NAME, self.scale_image_down(blob_marked , 900))
         cv2.waitKey(50)
+
+        if not keypoints :
+            return
+
+        # Can't do anything without an image
+        if len(self.depth_image_deque) < 1 :
+            return
+        # Try to map it to depth image if there exists keypoints
+        
+        
+        keypoint_time = RosTime.from_msg(msg.header.stamp).nanoseconds * 1e-9
+            
+         
+        depth_image = self.depth_image_deque.pop()
+        cv_depth_image = self.br.imgmsg_to_cv2(depth_image, depth_image.encoding)
+        
+        
+        # Assume intrinsics are received
+        i=0 
+        for keypoint in keypoints:
+            px, py = keypoint.pt
+            px= int(px)
+            py = int(py)
+
+            depth = cv_depth_image[py,px]
+            print(f"---\nAt pixel {px} ,{py}, Depth: {depth}")
+            sx,sy,sz = rs2.rs2_deproject_pixel_to_point(self.intrinsics, [px, py], depth)
+            print(f"deprojected space loc is {sx} , {sy} , {sz} ")    
+
+            tf = TransformStamped()
+            tf.header.stamp = depth_image.header.stamp
+            tf.header.frame_id = depth_image.header.frame_id
+            tf.child_frame_id = f"keypoint_{i}"
+            tf.transform.translation.x = sx/1000
+            tf.transform.translation.y = sy/1000
+            tf.transform.translation.z = sz/1000
+            self.transform_broadcaster.sendTransform(tf)
+            i+=1
+        self.transform_broadcaster.sendTransform
+
+
+    def record_camera_info_once(self) ->rs2.intrinsics:
+        print(f"Waiting for camera info")
+        have_msg , camera_info =wait_for_message(CameraInfo,self, "/D435i/aligned_depth_to_color/camera_info",time_to_wait=10)
+
+        print(f"Got camera info")
+        if have_msg is None:
+            raise RuntimeError("Did not get camera info after a long wait")
+            return
+        intrinsics = rs2.intrinsics()
+        intrinsics.width = camera_info.width
+        intrinsics.height = camera_info.height
+        intrinsics.ppx = camera_info.k[2]
+        intrinsics.ppy = camera_info.k[5]
+        intrinsics.fx = camera_info.k[0]
+        intrinsics.fy = camera_info.k[4]
+        if camera_info.distortion_model == 'plumb_bob':
+            intrinsics.model = rs2.distortion.brown_conrady
+        elif camera_info.distortion_model == 'equidistant':
+            intrinsics.model = rs2.distortion.kannala_brandt4
+        intrinsics.coeffs = [i for i in camera_info.d]
+
+        return intrinsics
+
+    def depth_image_cb(self, depth_image):
+        self.depth_image_deque.appendleft(depth_image)
+
+    def find_ring_process_timercb(self):
+        pass
+
+
+
+    def update_field_callback(self, filter: FilterBase, field_name: str, new_value):
+        # In theory this is actually very very bad. But in practice it worked.
+        # Problem with cv2 setTrackbarPos is it also trigger callback (instead of
+        # just user hitting it)
+        # So this would have cause a infinite loop
+        # In practice, the callback is only called when value is changed.
+        # So what will happen is a callback to change h_high will not re-trigger itself
+        # internally. However this this h_high callback also change h_low, it will
+        # trigger a h_low callback. Luckly, since value is set, setting h_low won't trigger
+        # another h_high callback
+
+        linked_attrs = filter.update_field_by_name(field_name, new_value)
+        actual_set_value = getattr(filter, field_name)
+        print(f"Setting {field_name} to {actual_set_value}")
+        cv2.setTrackbarPos(field_name, self.WINDOW_NAME, actual_set_value)
+
+        for linked_attr in linked_attrs:
+            print(f"Also update linked attr {linked_attr}")
+            linked_value = getattr(filter, linked_attr)
+            cv2.setTrackbarPos(linked_attr, self.WINDOW_NAME, linked_value)
+
+    def setup_cv_trackbar(self, filter: FilterBase):
+        default_value_map = {}
+        for field in dataclasses.fields(filter):
+            name = field.name
+            print(f"\n-------\n  Adding field {name}")
+            field_max, field_min = filter.get_field_range(name)
+            # Shouldn't use lambda here, as lambda creates links not copies
+            # binded_callback = lambda value: self.update_field_callback(name,value)
+
+            # Note this binding is in orders, from left to right.
+            binded_callback = functools.partial(self.update_field_callback, filter, name)
+
+            print(f"max min {field_max} , {field_min}\n{binded_callback}")
+
+            # When creating trackbar with min bigger then 0, or max smaller then 0.
+            # the creating process will call setTrackbar to snap it to limit, but
+            # will trigger a callback. Thus
+            # The default value needs to to be remembered.
+            default_value_map[name] = getattr(filter, name)
+            # print(f"callback {binded_callback}")
+            cv2.createTrackbar(name, self.WINDOW_NAME, field_min, field_max, binded_callback)
+
+        for field in dataclasses.fields(filter):
+            # Must do this after all bars being done. Or the set trackbar could trigger
+            # a callback which cause the linked_attr's trackbar (not existing) to be sets
+            print(f"-- Initializeing {field.name} to {default_value_map[field.name]} ")
+            cv2.setTrackbarPos(field.name, self.WINDOW_NAME, default_value_map[field.name])
+
+    def scale_image_down(self, image: np.ndarray , expected_width:float):
+        height, width, _ = image.shape
+
+        scale_down_factor = expected_width / width
+        return cv2.resize(image, (0, 0), fx=scale_down_factor, fy=scale_down_factor)
+
 
 if __name__ == '__main__':
     main()
