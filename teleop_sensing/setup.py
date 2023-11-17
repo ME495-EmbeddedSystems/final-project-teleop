ce651a5e8f2a18b8ee80cdf4e3231207b1bfbdd6
Leo <leograyc@gmail.com>
2023-11-16 21:15:13 -0600

    April tag working  (#2)
    * Real-sense and april tag nodes works together now.
    
    * April tag rect working now.
    
    * April tag reading works now.
    
    * Add more tags to the config
    
    ---------
    
    Co-authored-by: leo-chen <S8O9R6@u.northwestern.edu>

--- teleop_sensing/setup.py
+++ teleop_sensing/setup.py
@@ -2,6 +2,26 @@ from setuptools import find_packages, setup
 
 package_name = 'teleop_sensing'
 
+import pathlib
+here = pathlib.Path(__file__).parent.resolve()
+
+def copy_dir_rec(dest:pathlib.Path , source:pathlib.Path , here:pathlib.Path ) -> list[tuple[pathlib.Path , list[pathlib.Path]]]:
+    # Set dest.parent as root, then go through all sub files and output their p.relative_to(dest.parents)
+    if not source.is_dir():
+        return [(str(dest/ source.relative_to(here).parent) , [str(source)])]
+    out_list = []
+    current_level_list = (str(dest / source.relative_to(here)) , [])
+    for item in source.iterdir():
+        if item.is_dir():
+            out_list.extend(copy_dir_rec(dest,item,here))
+        else:
+            current_level_list[1].append(str(item.relative_to(here)))
+
+    if current_level_list[1] :
+        out_list.append(current_level_list)
+    return out_list
+
+
 setup(
     name=package_name,
     version='0.0.0',
@@ -10,7 +30,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-    ],
+    ]+
+    copy_dir_rec(pathlib.Path('share/' + package_name), here / pathlib.Path('launch'), here)+
+    copy_dir_rec(pathlib.Path('share/' + package_name), here / pathlib.Path('config'), here) ,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='rohan',
@@ -19,7 +41,6 @@ setup(
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-        ],
+        'console_scripts': ['cv_process = teleop_sensing.cv_process:main'],
     },
 )
