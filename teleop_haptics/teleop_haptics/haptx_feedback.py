a835884d6374b623484e1b2f66b9748cd371ee64
rohankota <rohankota@u.northwestern.edu>
2023-11-20 13:36:31 -0600

    added haptx mapping from gazebo wrenches

--- teleop_haptics/teleop_haptics/haptx_feedback.py
+++ teleop_haptics/teleop_haptics/haptx_feedback.py
@@ -13,7 +13,9 @@ Parameters
 """
 import rclpy
 from rclpy.node import Node
-from haptx_interfaces.msg import TactorGroupState
+from array import array
+from geometry_msgs.msg import WrenchStamped
+from haptx_interfaces.msg import TactorState
 
 
 class HaptxFeedback(Node):
@@ -34,13 +36,12 @@ class HaptxFeedback(Node):
 
         # Create publishers for tactor groups
         if self.use_right:
-            self.rh_tactor_pub = self.create_publisher(TactorGroupState, "haptx/rh/tactor_group_state", 10)
+            self.rh_tactor_pub = self.create_publisher(TactorState, "haptx/rh/tactor_state", 10)
         
         if self.use_left:
-            self.lh_tactor_pub = self.create_publisher(TactorGroupState, "haptx/lh/tactor_group_state", 10)
+            self.lh_tactor_pub = self.create_publisher(TactorState, "haptx/lh/tactor_state", 10)
 
-        self.inflation = 0.0
-        self.dir = 'up'
+        self.contact_wrench_sub = self.create_subscription(WrenchStamped, "force_torque", self.contact_wrench_callback, 10)
 
         # Center tactors
         self.rh_th_center_tactors = [61, 71, 81, 31, 41, 101, 91]
@@ -48,31 +49,93 @@ class HaptxFeedback(Node):
         self.rh_mf_center_tactors = [2061, 2071, 2081, 2031, 2041, 2101, 2091]
         self.rh_rf_center_tactors = [3032, 3031, 3022, 3052]
         self.rh_lf_center_tactors = [4032, 4031, 4022, 4052]
+        self.rh_center_tactors = [self.rh_ff_center_tactors, self.rh_mf_center_tactors, self.rh_rf_center_tactors, self.rh_lf_center_tactors, self.rh_th_center_tactors]
 
         self.lh_th_center_tactors = [10061, 10071, 10081, 10031, 10041, 10101, 10091]
         self.lh_ff_center_tactors = [11061, 11071, 11081, 11031, 11041, 11101, 11091]
         self.lh_mf_center_tactors = [12061, 12071, 12081, 12031, 12041, 12101, 12091]
         self.lh_rf_center_tactors = [13032, 13031, 13022, 13052]
         self.lh_lf_center_tactors = [14032, 14031, 14022, 14052]
+        self.lh_center_tactors = [self.lh_ff_center_tactors, self.lh_mf_center_tactors, self.lh_rf_center_tactors, self.lh_lf_center_tactors, self.lh_th_center_tactors]
+        
+        # Peripheral tactors
+        self.rh_th_peripheral_tactors = [11, 12, 13, 21, 51, 111, 121, 122, 123]
+        self.rh_ff_peripheral_tactors = [1011, 1012, 1013, 1021, 1051, 1111, 1121, 1122, 1123]
+        self.rh_mf_peripheral_tactors = [2011, 2012, 2013, 2021, 2051, 2111, 2121, 2122, 2123]
+        self.rh_rf_peripheral_tactors = [3011, 3021, 3041, 3051, 3061]
+        self.rh_lf_peripheral_tactors = [4011, 4021, 4041, 4051, 4061]
+        self.rh_peripheral_tactors = [self.rh_ff_peripheral_tactors, self.rh_mf_peripheral_tactors, self.rh_rf_peripheral_tactors, self.rh_lf_peripheral_tactors, self.rh_th_peripheral_tactors]
+
+        self.lh_th_peripheral_tactors = [1011, 1012, 1013, 1021, 1051, 10111, 10121, 10122, 10123]
+        self.lh_ff_peripheral_tactors = [11011, 11012, 11013, 11021, 11051, 11111, 11121, 11122, 11123]
+        self.lh_mf_peripheral_tactors = [12011, 12012, 12013, 12021, 12051, 12111, 12121, 12122, 12123]
+        self.lh_rf_peripheral_tactors = [13011, 13021, 13041, 13051, 13061]
+        self.lh_lf_peripheral_tactors = [14011, 14021, 14041, 14051, 14061]
+        self.lh_peripheral_tactors = [self.lh_ff_peripheral_tactors, self.lh_mf_peripheral_tactors, self.lh_rf_peripheral_tactors, self.lh_lf_peripheral_tactors, self.lh_th_peripheral_tactors]
+
+        # Maps finger topic to index of finger in tactor lists above
+        self.finger_topic_map = {
+            'right_hand/thumb_2_thumb_tip/force_torque_sensor': 4,
+            'right_hand/index_3_index_tip/force_torque_sensor': 0,
+            'right_hand/middle_3_middle_tip/force_torque_sensor': 1,
+            'right_hand/ring_3_ring_tip/force_torque_sensor': 2,
+            'right_hand/pinky_3_pinky_tip/force_torque_sensor': 3,
+            'left_hand/thumb_2_thumb_tip/force_torque_sensor': 4,
+            'left_hand/index_3_index_tip/force_torque_sensor': 0,
+            'left_hand/middle_3_middle_tip/force_torque_sensor': 1,
+            'left_hand/ring_3_ring_tip/force_torque_sensor': 2,
+            'left_hand/pinky_3_pinky_tip/force_torque_sensor': 3
+        }
+
+        self.rh_contact_vals = [0.0, 0.0, 0.0, 0.0, 0.0]
+        self.lh_contact_vals = [0.0, 0.0, 0.0, 0.0, 0.0]
 
     def timer_callback(self):
         """Timer function for the Haptx Feedback node."""
-        msg = TactorGroupState()
-        msg.tactor_group = ['lh_th', 'lh_mf', 'lh_rf']
-        msg.inflation = [self.inflation, self.inflation, self.inflation]
 
-        self.lh_tactor_pub.publish(msg)
+        if self.use_right:
+            tactor_msg = TactorState()
+            tactor_msg.tactor_id = []
+            tactor_msg.inflation = []
 
-        if self.dir == 'up':
-            if self.inflation < 1.0:
-                self.inflation += 0.05
+            for i in range(5):
+                # Inflate center tactors fully
+                tactor_msg.tactor_id += array('i', self.rh_center_tactors[i])
+                tactor_msg.inflation += array('d', [1.0]*len(self.rh_center_tactors[i]))
+
+                # Inflate peripheral tactor proportionally
+                tactor_msg.tactor_id += array('i', self.rh_peripheral_tactors[i])
+                tactor_msg.inflation += array('d', [self.rh_contact_vals[i]]*len(self.rh_peripheral_tactors[i]))
+
+            self.rh_tactor_pub.publish(tactor_msg)
+
+        if self.use_left:
+            tactor_msg = TactorState()
+            tactor_msg.tactor_id = []
+            tactor_msg.inflation = []
+
+            for i in range(5):
+                # Inflate center tactors fully
+                tactor_msg.tactor_id += array('i', self.lh_center_tactors[i])
+                tactor_msg.inflation += array('d', [1.0]*len(self.lh_center_tactors[i]))
+
+                # Inflate peripheral tactor proportionally
+                tactor_msg.tactor_id += array('i', self.lh_peripheral_tactors[i])
+                tactor_msg.inflation += array('d', [self.lh_contact_vals[i]]*len(self.lh_peripheral_tactors[i]))
+        
+            self.lh_tactor_pub.publish(tactor_msg)
+
+    def contact_wrench_callback(self, msg):
+        finger_frame = msg.header.frame_id
+        finger_index = self.finger_topic_map[finger_frame]
+
+        normal_force = -msg.wrench.force.z
+
+        if normal_force > 0.01:
+            if finger_frame[0] == 'r':
+                self.rh_contact_vals[finger_index] = min(1.0, normal_force/0.98)
             else:
-                self.dir = 'down'
-        elif self.dir == 'down':
-            if self.inflation > 0.0:
-                self.inflation -= 0.05
-            else:
-                self.dir = 'up'
+                self.lh_contact_vals[finger_index] = min(1.0, normal_force/0.98)
 
 
 def main(args=None):
