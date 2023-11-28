cf8d0b5f82d5f4ea89198d8b6781bfe79448ce26
rohankota <rohankota@u.northwestern.edu>
2023-11-27 18:12:16 -0600

    added visualization package

--- teleop_visualization/setup.py
+++ teleop_visualization/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-package_name = 'rviz_rings'
+package_name = 'teleop_visualization'
 
 setup(
     name=package_name,
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'rings = rviz_rings.rings:main'
+            'rings = teleop_visualization.rings:main'
         ],
     },
 )
