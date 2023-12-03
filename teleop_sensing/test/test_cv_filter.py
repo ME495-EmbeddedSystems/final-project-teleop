baf86884ad085af9387d6b7a0414ddb3d85c23b3
leo-chen <S8O9R6@u.northwestern.edu>
2023-12-03 15:48:34 -0600

    formatting test

--- teleop_sensing/test/test_cv_filter.py
+++ teleop_sensing/test/test_cv_filter.py
@@ -4,6 +4,7 @@ from teleop_sensing import cv_filers
 import dataclasses
 import cv2
 
+
 def test_filter_base():
     filter_base_only = cv_filers.FilterBase()
 
@@ -11,30 +12,29 @@ def test_filter_base():
     with pytest.raises(ValueError):
         filter_base_only.get_field_range("anything")
     with pytest.raises(ValueError):
-        filter_base_only.update_field_by_name("anything" , 0)
+        filter_base_only.update_field_by_name("anything", 0)
 
 
 def test_hsv_filter():
-    hsv_filter = cv_filers.HSVFilter(20,10,20,10,20,10)
+    hsv_filter = cv_filers.HSVFilter(20, 10, 20, 10, 20, 10)
 
-    
-    hsv_filter.update_field_by_name("h_high" , 30)
+    hsv_filter.update_field_by_name("h_high", 30)
     assert hsv_filter.h_high == 30
-    linked_filed = hsv_filter.update_field_by_name("h_high" , 5)    
+    linked_filed = hsv_filter.update_field_by_name("h_high", 5)
     assert linked_filed == ["h_low"]
     assert hsv_filter.h_high > hsv_filter.h_low
-    linked_filed = hsv_filter.update_field_by_name("v_low" , 100)    
+    linked_filed = hsv_filter.update_field_by_name("v_low", 100)
     assert linked_filed == ["v_high"]
     assert hsv_filter.v_high > hsv_filter.v_low
 
 
 def test_setup_trackbar():
     test_cv2_window = "test_window"
-    h_filter = cv_filers.HOnlyFilter(100,10)
+    h_filter = cv_filers.HOnlyFilter(100, 10)
 
     helper = cv_filers.TrackBarHelper()
-    helper.setup_cv_trackbar(h_filter,window_name="test_window")
+    helper.setup_cv_trackbar(h_filter, window_name="test_window")
 
     for field in dataclasses.fields(h_filter):
         name = field.name
-        assert cv2.getTrackbarPos(name,test_cv2_window) == getattr(h_filter, name)
+        assert cv2.getTrackbarPos(name, test_cv2_window) == getattr(h_filter, name)
